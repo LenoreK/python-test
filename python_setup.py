7d53484d72378c3fd269aac8dbedd4084dfbc232
Lenore Kelly <lenorekelly2@gmail.com>
2022-05-31 14:26:16 -0400

    first commit

---
+++ python_setup.py
@@ -0,0 +1,14 @@
+print("Hello from inside a file!")
+
+def hello():
+    print("Hello from a function")
+
+hello()
+
+def pack(fname, lname, age):
+    print(fname + " " + lname + "is " + age + " years old")
+
+pack(lenore, kelly, 27)
+
+def eat_lunch(food1, food2):
+    print("First I eat " + food1 + "." + "Next I eat " + food2)
