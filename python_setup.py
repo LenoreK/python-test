b81c068f68870e07bd5401700cce7ec2c029c4ef
Lenore Kelly <lenorekelly2@gmail.com>
2022-05-31 18:39:25 -0400

    fixed strings

--- python_setup.py
+++ python_setup.py
@@ -8,7 +8,7 @@ hello()
 def pack(fname, lname, age):
     print(fname + " " + lname + "is " + age + " years old")
 
-pack(lenore, kelly, 27)
+pack("lenore", "kelly", 27)
 
 def eat_lunch(food1, food2):
     print("First I eat " + food1 + "." + "Next I eat " + food2)
